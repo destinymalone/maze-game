10dadaa642bf6ffa056368a4779e6cbb3ad05176
Destiny Malone <dmalone@basecampcodingacademy.org>
2020-02-04 11:22:04 -0600

    Updated README.md

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const canvas = document.getElementById('canvas')
 const context = canvas.getContext('2d')
-
-
 const worldMap = [
     [1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
     [1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1],
